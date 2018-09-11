e0b153c637dacb8391c1c089638da1153a06a7a9
cy <cy118@naver.com>
2018-09-11 17:17:04 +0900

    follow 관련 test작성

---
+++ src/test/java/com/cyhee/rabit/follow/FollowServiceTest.java
@@ -0,0 +1,88 @@
+package com.cyhee.rabit.follow;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.cyhee.rabit.model.follow.Follow;
+import com.cyhee.rabit.model.follow.FollowStatus;
+import com.cyhee.rabit.model.user.User;
+import com.cyhee.rabit.service.follow.BasicFollowService;
+import com.cyhee.rabit.service.follow.FollowService;
+import com.cyhee.rabit.service.user.BasicUserService;
+import com.cyhee.rabit.service.user.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DataJpaTest
+@Import({BasicUserService.class, BCryptPasswordEncoder.class, BasicUserService.class, BasicFollowService.class})
+public class FollowServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private FollowService followService;
+
+    User user1;
+    User user2;
+    User user3;
+    Follow follow1;
+
+    @Before
+    public void setup() {
+        user1 = new User().setEmail("email1@com").setPassword("password1@").setUsername("user1");
+        user2 = new User().setEmail("email2@com").setPassword("password2@").setUsername("user2");
+        user2 = new User().setEmail("email3@com").setPassword("password3@").setUsername("user3");
+
+        follow1 = new Follow().setFollower(user1).setFollowee(user2);
+    }
+
+    @Test
+    public void createAndGet() {
+        userService.addUser(user1);
+        userService.addUser(user2);
+        followService.addFollow(follow1);
+
+        Follow follow = followService.getFollow(follow1.getId());
+
+        assertThat(follow)
+            .isEqualTo(follow1);
+    }
+
+    @Test
+    public void update() {
+        userService.addUser(user1);
+        userService.addUser(user2);
+        followService.addFollow(follow1);
+
+        Follow form = new Follow().setFollowee(user3);
+
+        followService.updateFollow(follow1.getId(), form);
+
+        assertThat(follow1)
+            .extracting(Follow::getFollowee)
+            .containsExactly(form.getFollowee());
+    }
+
+    @Test
+    public void deleteAndGet() {
+        Follow source = new Follow().setStatus(FollowStatus.INACTIVE);
+
+        userService.addUser(user1);
+        userService.addUser(user2);
+        followService.addFollow(follow1);
+
+        followService.deleteFollow(follow1.getId());
+
+        assertThat(follow1)
+            .extracting(Follow::getStatus)
+            .containsExactly(source.getStatus());
+    }
+}
