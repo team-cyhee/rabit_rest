1bd9c5202cce4ca5fe6e527507cf0c71b090df18
chy <whgksdyd112@naver.com>
2018-09-26 20:01:36 +0900

    Jenkinsfile git checkout again

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,8 @@
 node {  
+  stage('Checkout') {
+    git branch: 'master', credentialsId: '1c530311be4bbab34809d65af76a5424e2c787a6', url: 'https://github.com/team-cyhee/rabit_rest.git'
+  }
+  
   stage('Bulid') {
     sh 'ls'
     sh 'pwd'
