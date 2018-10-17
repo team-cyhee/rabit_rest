85aa1240c351b29cf7c8d135be3c0f769b25ec58
chy <whgksdyd112@naver.com>
2018-10-17 13:59:36 +0900

    Support post in store controller

--- src/main/java/com/cyhee/rabit/web/goal/GoalStoreController.java
+++ src/main/java/com/cyhee/rabit/web/goal/GoalStoreController.java
@@ -9,18 +9,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cyhee.rabit.model.cmm.ContentStatus;
+import com.cyhee.rabit.model.cmm.ContentType;
 import com.cyhee.rabit.model.comment.Comment;
 import com.cyhee.rabit.model.goal.Goal;
 import com.cyhee.rabit.model.goallog.GoalLog;
 import com.cyhee.rabit.model.like.Like;
 import com.cyhee.rabit.model.user.User;
 import com.cyhee.rabit.service.cmm.AuthHelper;
+import com.cyhee.rabit.service.comment.CommentService;
 import com.cyhee.rabit.service.goal.GoalService;
 import com.cyhee.rabit.service.goal.GoalStoreService;
+import com.cyhee.rabit.service.goallog.GoalLogService;
 import com.cyhee.rabit.service.like.LikeService;
 import com.cyhee.rabit.service.user.UserService;
 
@@ -35,6 +39,10 @@ public class GoalStoreController {
 	private UserService userService;
 	@Autowired
 	private LikeService likeService;
+	@Autowired
+	private CommentService commentService;
+	@Autowired
+	private GoalLogService goalLogService;
 	
 	
 	@GetMapping("/goallogs")
@@ -43,11 +51,36 @@ public class GoalStoreController {
         return new ResponseEntity<>(goalStoreService.getGoalLogs(goal, ContentStatus.all(), pageable), HttpStatus.OK);
     }
 	
+	@PostMapping("/goallogs")
+	public ResponseEntity<Void> addGoalLog(@PathVariable Long id, @RequestBody GoalLog goalLog) {
+		Goal goal = goalService.getGoal(id);
+    	
+    	goalLog.setGoal(goal);
+    	
+    	goalLogService.addGoalLog(goalLog);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
 	@GetMapping("/comments")
 	public ResponseEntity<Page<Comment>> getComments(@PathVariable Long id, @PageableDefault Pageable pageable) {
 		Goal goal = goalService.getGoal(id);
         return new ResponseEntity<>(goalStoreService.getComments(goal, pageable), HttpStatus.OK);
     }
+	
+	@PostMapping("/comments")
+	public ResponseEntity<Void> addComment(@PathVariable Long id, @RequestBody Comment comment) {
+		Goal goal = goalService.getGoal(id);
+		
+    	String username = AuthHelper.getUsername();
+    	User author = userService.getUserByUsername(username);
+    	
+    	comment.setAuthor(author);
+    	comment.setParentId(goal.getId());
+    	comment.setType(ContentType.GOAL);
+    	
+    	commentService.addComment(comment);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 
     @GetMapping("/likes")
 	public ResponseEntity<Page<Like>> getLikes(@PathVariable Long id, @PageableDefault Pageable pageable) {
--- src/main/java/com/cyhee/rabit/web/goallog/GoalLogStoreController.java
+++ src/main/java/com/cyhee/rabit/web/goallog/GoalLogStoreController.java
@@ -9,14 +9,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cyhee.rabit.model.cmm.ContentType;
 import com.cyhee.rabit.model.comment.Comment;
 import com.cyhee.rabit.model.goallog.GoalLog;
 import com.cyhee.rabit.model.like.Like;
 import com.cyhee.rabit.model.user.User;
 import com.cyhee.rabit.service.cmm.AuthHelper;
+import com.cyhee.rabit.service.comment.CommentService;
 import com.cyhee.rabit.service.goallog.GoalLogService;
 import com.cyhee.rabit.service.goallog.GoalLogStoreService;
 import com.cyhee.rabit.service.like.LikeService;
@@ -33,12 +36,29 @@ public class GoalLogStoreController {
 	private UserService userService;
 	@Autowired
 	private LikeService likeService;
+	@Autowired
+	private CommentService commentService;
 	
 	@GetMapping("/comments")
 	public ResponseEntity<Page<Comment>> getComments(@PathVariable Long id, @PageableDefault Pageable pageable) {
 		GoalLog goalLog = goalLogService.getGoalLog(id);
         return new ResponseEntity<>(goalLogStoreService.getComments(goalLog, pageable), HttpStatus.OK);
     }
+	
+	@PostMapping("/comments")
+	public ResponseEntity<Void> addComment(@PathVariable Long id, @RequestBody Comment comment) {
+		GoalLog goalLog = goalLogService.getGoalLog(id);
+		
+    	String username = AuthHelper.getUsername();
+    	User author = userService.getUserByUsername(username);
+    	
+    	comment.setAuthor(author);
+    	comment.setParentId(goalLog.getId());
+    	comment.setType(ContentType.GOALLOG);
+    	
+    	commentService.addComment(comment);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 
 	@GetMapping("/likes")
 	public ResponseEntity<Page<Like>> getLikes(@PathVariable Long id, @PageableDefault Pageable pageable) {
